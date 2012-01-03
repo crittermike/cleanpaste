10ee9dd33e7a6b7e69c0f097c5ee11434285e786
Mike Crittenden <mikecrittenden@gmail.com>
2012-01-03 15:20:23 -0500

    remove unnecessary defpartial

--- src/cleanpaste/views/cleaner.clj
+++ src/cleanpaste/views/cleaner.clj
@@ -10,15 +10,12 @@
       (clojure.string/replace text #"[^\u0000-\u007F]" "")
       text))
 
-(defpartial cleaner-form [{:keys [paste]}]
-  (text-area "paste" (clean-text paste))
-  (submit-button "Clean it up!"))
-
 (defpage "/" {:as result}
   (common/layout 
     [:p "Remove all non-ASCII characters from text."]
     (form-to [:post "/"]
-             (cleaner-form result))))
+             (text-area "paste" (clean-text (get result :paste)))
+             (submit-button "Clean it up!"))))
 
 (defpage [:post "/"] {:as result}
   (render "/" result))
